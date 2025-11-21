240f4acd31510ea6ee69fc4d72a6a7d948e0c6e9
senpai0807 <jedikun98@gmail.com>
2025-11-20 23:59:06 -0500

    fix package name and export types

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
-package lunar_go_sdk
+package lunartools
 
-import "github.com/lunartools/lunar-go-sdk/src/client"
+import (
+	"github.com/lunartools/lunar-go-sdk/src/client"
+	"github.com/lunartools/lunar-go-sdk/src/types"
+)
 
 type Client = client.Client
+type Config = types.Config
+type Webhook = types.Webhook
+type Embed = types.Embed
+type Field = types.Field
+type Footer = types.Footer
+type Author = types.Author
+type Thumbnail = types.Thumbnail
+type Image = types.Image
+type WebhookResponse = types.WebhookResponse
+type AddProduct = types.AddProduct
+type AddOrder = types.AddOrder
 
 var NewClient = client.NewClient
 
